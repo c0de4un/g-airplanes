012f38e3abe947481ac9d2e48ed8898833fdc990
Denis <code4un@yandex.ru>
2020-03-19 17:17:51 +0400

    Add files via upload

---
+++ planes.php
@@ -0,0 +1,46 @@
+<?php
+
+	abstract class Airplane
+	{
+		
+		public abstract function TakeOff();
+		public abstract function Land();
+		
+	}
+
+	class Mig extends Airplane
+	{
+		
+		public function TakeOff()
+		{
+			echo "Миг - Взлет";
+		}
+		
+		public function Land()
+		{
+			echo "Миг - Посадка";
+		}
+		
+		public function Attack()
+		{
+			echo "Миг - Атака";
+		}
+		
+	}
+
+	class Ty154 extends Airplane
+	{
+		
+		public function TakeOff()
+		{
+			echo "ТУ-154 - Взлет";
+		}
+		
+		public function Land()
+		{
+			echo "ТУ-154 - Посадка";
+		}
+		
+	}
+
+?>
